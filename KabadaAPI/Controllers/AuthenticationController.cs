a661e1076db70a4f8f03489d65e59b3a5e20d87a
Mindaugas Liogys <m.liogys@eif.viko.lt>
2020-04-21 18:33:11 +0300

    Added basic controllers functionality

---
+++ KabadaAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KabadaAPI.ViewModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KabadaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        [Route("login/empty")]
+        public IActionResult Get()
+        {
+            return BadRequest("Bad request");
+        }
+
+        [HttpGet]
+        [Route("login/{name}")]
+        public IActionResult Get(string name)
+        {
+            if (name.Equals("Mindaugas"))
+                return Ok("Login success");
+            else
+                return BadRequest("Login failed");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Get([FromBody]User user)
+        {
+            return Ok(user);
+        }
+    }
+}
---
+++ KabadaAPI/ViewModels/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace KabadaAPI.ViewModels
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
