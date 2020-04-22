84bdb55141a0cfd997d7bf6c7e15067bd2644008
Mindaugas Liogys <m.liogys@eif.viko.lt>
2020-04-22 12:01:02 +0300

    ConnectionStrings refactored one more time

--- KabadaAPI.DataSource/Utilities/ConnectionStrings.cs
+++ KabadaAPI.DataSource/Utilities/ConnectionStrings.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+
 namespace KabadaAPI.DataSource.Utilities
 {
-    public class ConnectionStrings
+    public static class ConnectionStrings
     {
-        public ConnectionStrings()
-        {
-        }
+        public static string SQLServer = @"Data Source=localhost; Initial Catalog=KabadaDB; User Id=use_your_own_name; Password=use_your_own_password;";
+        // @"Data Source=.\sqlexpress; Initial Catalog=KabadaDB; Integrated Security=SSPI;" 
     }
 }
