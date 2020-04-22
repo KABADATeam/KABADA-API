10fdd74d043395fb624a485e4fcd042c5a34280d
mliogys <m.liogys@eif.viko.lt>
2020-04-22 14:34:17 +0300

    Add files via upload

---
+++ KabadaAPI.DataSource/ConnectionStrings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace KabadaAPI.DataSource.Utilities
+{
+    public static class ConnectionStrings
+    {
+        //public static string SQLServer = @"Data Source=localhost; Initial Catalog=KabadaDB; User Id=<userName>; Password=<password>;";
+        public static string SQLServer = @"Data Source=.\sqlserver; Initial Catalog=KabadaDB; Integrated Security=SSPI;";
+    }
+}
