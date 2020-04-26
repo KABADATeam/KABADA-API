98f5c69a76ceecf69af933476f3d3172b4d90277
Mindaugas Liogys <m.liogys@eif.viko.lt>
2020-04-26 23:36:59 +0300

    Added CORS configuration

---
+++ KabadaAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KabadaAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy", builder => {
+
+                    builder.WithOrigins("http://localhost:3000").AllowAnyMethod();
+                    builder.WithHeaders("Access-Control-Allow-Origin", "*");
+                    builder.WithHeaders("Access-Control-Allow-Headers", "Authorization, Origin, WWW-Authenticate, Content-Length, X-Requested-With, Content-Type, Accept");
+                });
+            });
+        }
+    }
+}
--- KabadaAPI/Startup.cs
+++ KabadaAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KabadaAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,7 @@ namespace KabadaAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
             services.AddControllers();
         }
 
@@ -35,6 +37,8 @@ namespace KabadaAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("MyPolicy");
 
             app.UseHttpsRedirection();
 
