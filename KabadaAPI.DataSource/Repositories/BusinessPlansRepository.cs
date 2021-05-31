f4b32fc106906a9913e7bdbe83d7cd065e24d305
ennag <enna.golubova@sets.lv>
2021-05-31 16:03:35 +0300

    PublicBusinessPlan classes

---
+++ KabadaAPI.DataSource/Repositories/BusinessPlansRepository.cs
@@ -0,0 +1,18 @@
+﻿using KabadaAPI.DataSource.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KabadaAPI.DataSource.Repositories
+{
+    public class BusinessPlansRepository : BaseRepository
+    {
+
+        public List<BusinessPlan> GetPublicPlans()
+        {
+            return context.BusinessPlans.Where(x => x.Public == true)
+                   .OrderBy(x => x.Title)
+                   .ToList();
+        }
+        
+    }
+}
---
+++ KabadaAPI/ViewModels/PublicBusinessPlans.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KabadaAPI.ViewModels
+{
+    public class PublicBusinessPlans
+    {        
+        public List<PublicBusinessPlan> BusinessPlan { get; set; }
+    }
+}
