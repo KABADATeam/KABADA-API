b301c86a9e1f7e7eb91023b6e72a1b3fdc5df79d
juris.strods@sets.lv <Juris.Strods@sets.lv>
2021-06-07 14:28:22 +0300

    added SWOT texting support

--- KabadaAPI.DataSource/Migrations/20210607112620_AddSWOTtexting.Designer.cs
+++ KabadaAPI.DataSource/Migrations/20210607112620_AddSWOTtexting.Designer.cs
@@ -4,14 +4,16 @@ using KabadaAPI.DataSource;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KabadaAPI.DataSource.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210607112620_AddSWOTtexting")]
+    partial class AddSWOTtexting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
