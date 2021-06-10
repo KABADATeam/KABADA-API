31f40f9f714ae173702b24f6308d868cc0b205c1
juris.strods@sets.lv <Juris.Strods@sets.lv>
2021-06-10 18:37:13 +0300

    removed entity Plan_SWOT

--- KabadaAPI.DataSource/Models/Plan_SWOT.cs
+++ KabadaAPI.DataSource/Models/Plan_SWOT.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿//using System;
+//using System.ComponentModel.DataAnnotations;
 
-namespace KabadaAPI.DataSource.Models {
-  public class Plan_SWOT {
-        [Key]
-        public Guid Id { get; set; }
+//namespace KabadaAPI.DataSource.Models {
+//  public class Plan_SWOT {
+//        [Key]
+//        public Guid Id { get; set; }
 
-        [Required]
-        public Guid BusinessPlanId { get; set; }
-        public BusinessPlan BusinessPlan { get; set; }
+//        [Required]
+//        public Guid BusinessPlanId { get; set; }
+//        public BusinessPlan BusinessPlan { get; set; }
 
-        [Required]
-        public Guid TexterId { get; set; }
-        public Texter Texter { get; set; }
+//        [Required]
+//        public Guid TexterId { get; set; }
+//        public Texter Texter { get; set; }
 
-        [Required]
-        [Range(1,2)]
-        public short Kind { get; set; }
-    }
-  }
+//        [Required]
+//        [Range(1,2)]
+//        public short Kind { get; set; }
+//    }
+//  }
