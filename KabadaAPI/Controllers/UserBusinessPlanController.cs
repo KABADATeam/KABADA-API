91c0802dd690c9e3ecaa42fbdf8b3a8fb1b845fd
ennag <enna.golubova@sets.lv>
2021-06-11 16:32:13 +0300

    comment for now

--- KabadaAPI/Controllers/UserBusinessPlanController.cs
+++ KabadaAPI/Controllers/UserBusinessPlanController.cs
@@ -107,10 +107,10 @@ namespace KabadaAPI.Controllers {
             repo.ChangeSwotCompleted(planUpdate.business_plan_id, planUpdate.is_swot_completed, uGuid);
             return Ok("Success");
         }
-        [Route("changeResourcesCompleted")]
-        [Authorize]
-        [HttpPost]
-        public IActionResult ChangeResourcesCompleted([FromBody] ChangePlanParameter planUpdate) { return prun<ChangePlanParameter>(_changeResourcesCompleted, planUpdate); }
+        //[Route("changeResourcesCompleted")]
+        //[Authorize]
+        //[HttpPost]
+        //public IActionResult ChangeResourcesCompleted([FromBody] ChangePlanParameter planUpdate) { return prun<ChangePlanParameter>(_changeResourcesCompleted, planUpdate); }
         private IActionResult _changeResourcesCompleted([FromBody] ChangePlanParameter planUpdate)
         {
             BusinessPlansRepository repo = new BusinessPlansRepository(config, _logger);
