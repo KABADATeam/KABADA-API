baaf2912eb3d98c811b16b400a58a2587d20854a
juris.strods@sets.lv <Juris.Strods@sets.lv>
2021-06-14 09:45:11 +0300

    .

--- KabadaAPI/Controllers/KeyResourcesController.cs
+++ KabadaAPI/Controllers/KeyResourcesController.cs
@@ -1,16 +1,18 @@
 ﻿using KabadaAPI.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace KabadaAPI.Controllers {
-  [Route("api/kRes")]
+  [Route("api/kres")]
   [ApiController]
   public class KeyResourcesController : KController {
     public KeyResourcesController(ILogger<KController> logger, IConfiguration configuration) : base(logger, configuration) {}
 
-    [HttpGet]
     [Route("categories")]
+    [Authorize]
+    [HttpGet]
     public IActionResult Categories() { return grun(_Categories); }
     private IActionResult _Categories() {
       var r=new ResourceCategories();
