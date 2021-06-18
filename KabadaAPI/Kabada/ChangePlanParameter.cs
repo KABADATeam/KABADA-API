ce1478d1dac76bc6ba7a7f09d7460bda4b34263e
ennag <enna.golubova@sets.lv>
2021-06-18 19:25:26 +0300

    toggling of is_partners_completed added

--- KabadaAPI/Kabada/ChangePlanParameter.cs
+++ KabadaAPI/Kabada/ChangePlanParameter.cs
@@ -5,5 +5,7 @@ namespace Kabada {
     public Guid business_plan_id { get; set; }
     public bool is_swot_completed { get; set; }
     public bool is_resources_completed { get; set; }
-  }
+    public bool is_partene_completed { get; set; }
+    
+}
 }
