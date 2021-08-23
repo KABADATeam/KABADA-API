9c00fcb4bb10455b9ed517c70419a517fec19d3d
jst <Juris.Strods@sets.lv>
2021-08-23 12:20:46 +0300

    .

--- KabadaAPI/BL/IndustryRisksManager.cs
+++ KabadaAPI/BL/IndustryRisksManager.cs
@@ -8,5 +8,22 @@ namespace KabadaAPI {
       if (f != null) return f.FullName; // return full path for the found
       return null; // nothing found
       }
+
+    public void proccessCsvFiles(string fullDirectoryPath){
+      while(true){
+        var f=getTheOldiest(fullDirectoryPath);
+        if(f==null)
+          break;
+        processSingleFile(f);
+        }
+      }
+
+    private void processSingleFile(string f) {
+      // file name ---> (set/DEL, List<isIndustry, i/a Guid>)
+      // load scsv. if good, save to a Texter
+      // create pointers to texter
+      // remove not referenced texters
+      throw new System.NotImplementedException();
+      }
     }
   }
