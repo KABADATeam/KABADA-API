ff1d4d110839dbea276049a626388c395c4c3e22
jst <Juris.Strods@sets.lv>
2021-09-21 17:23:01 +0300

    .

--- KabadaAPI/BL/BusinessPlanBL.cs
+++ KabadaAPI/BL/BusinessPlanBL.cs
@@ -178,12 +178,15 @@ namespace KabadaAPI {
     private List<CostBL> myVariableCost_s { get { return gA(PlanAttributeKind.variableCost).Select(x=>new CostBL(x, false)).ToList(); }}
     private List<CostBL> myCost_s { get {var r=myFixedCost_s; r.AddRange(myVariableCost_s); return r; }}
 
-    private List<CashFlowRow> initialRevenue { get {
+    private CashFlowTable initialRevenue { get {
+      var br=new CashFlowTable();
       var r=new List<CashFlowRow>();
+      br.rows=r;
       r.Add(new CashFlowRow("Owner's contribution", (e.startup.loan_amount==null || e.startup.total_investments==null)?null:e.startup.total_investments-e.startup.loan_amount));
       r.Add(new CashFlowRow("Loan from bank/leasing company", e.startup.loan_amount));
-      r.Add(new CashFlowRow("Total initial revenue", e.startup.total_investments));
-      return r;
+      
+      br.summeries=new List<CashFlowRow>(){ new CashFlowRow("Total initial revenue", e.startup.total_investments)};
+      return br;
       }}
     
     }
---
+++ KabadaAPI/Kabada/CashFlowTable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Kabada {
+  public class CashFlowTable {
+    public List<CashFlowRow> rows;
+    public List<CashFlowRow> summeries;
+    }
+  }
