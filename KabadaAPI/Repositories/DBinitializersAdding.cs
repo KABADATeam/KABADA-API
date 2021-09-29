f5f934c4b21af439a0a9fbf4c3cd6d72a1819333
ennag <enna.golubova@sets.lv>
2021-09-29 10:32:17 +0300

    ESSR initial data updated

--- KabadaAPI/Repositories/DBinitializersAdding.cs
+++ KabadaAPI/Repositories/DBinitializersAdding.cs
@@ -9,7 +9,7 @@ namespace KabadaAPI {
     private List<Func<Transactioner, bool>> initUpdates { get {
       return new List<Func<Transactioner, bool>>(){
 //===============================1========================================//
-        u_0, u_1, u_2, u_3, u_4 };
+        u_0, u_1, u_2, u_3, u_4, u_5 };
 //=======================================================================//
       }}
 
@@ -57,7 +57,11 @@ namespace KabadaAPI {
       var ctx = tr.Context;
       return importLoader(ctx);
       }
-
+    private bool u_5(Transactioner tr)
+    {
+        var ctx = tr.Context;
+        return importLoader(ctx);
+    }
 
         //=======================================================================//
 
