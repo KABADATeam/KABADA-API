ba2e5c8c0d9a7598566d95618e965a5a5e406d7a
ennag <enna.golubova@sets.lv>
2021-10-15 17:47:39 +0300

    Short term

--- KabadaAPI/Kabada/PlanStartupInvestmentBase.cs
+++ KabadaAPI/Kabada/PlanStartupInvestmentBase.cs
@@ -11,7 +11,10 @@
     //business financing part
     public short? payment_period; // any value from range: 6, 12, 18, ...., 120
     public decimal? interest_rate;
-    public short? grace_period;
+    public short? grace_period; //Max months: payment_period - 6 (12 months - 6 = 6 or 24 months  - 6 = 18), 0 by default
+    public short? payment_period_short; //Max months: Selected cash flow period - 1 (12 months - 1 = 11 or  24 months  - 1 = 23)
+    public decimal? interest_rate_short;
+    public short? grace_period_short; 
     //working capital part
     public decimal? working_capital_amount; 
     public decimal? startup_own_money;
