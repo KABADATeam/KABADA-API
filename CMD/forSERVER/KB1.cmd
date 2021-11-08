63ee1da42258d120915b7f50a88ecb06d28181e0
jst <Juris.Strods@sets.lv>
2021-11-08 08:36:37 +0200

    batch %date% is locale dependent - corrected for LV

--- CMD/forSERVER/KB1.cmd
+++ CMD/forSERVER/KB1.cmd
@@ -17,10 +17,9 @@ IF NOT EXIST "%W%" (
   goto :eof
 )
 
-set CUR_YYYY=%date:~10,4%
-set CUR_MM=%date:~4,2%
-set CUR_DD=%date:~7,2%
-set CUR_HH=%time:~0,2%
+set CUR_YYYY=%date:~6,4%
+set CUR_MM=%date:~3,2%
+set CUR_DD=%date:~0,2%
 if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
 set CUR_NN=%time:~3,2%
 set CUR_SS=%time:~6,2%
