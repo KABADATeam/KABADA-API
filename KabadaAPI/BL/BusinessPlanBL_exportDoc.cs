4b2686f4b98cced9827072e201a51c7a7528ffdc
jst <Juris.Strods@sets.lv>
2021-12-01 16:04:46 +0200

    .

---
+++ KabadaAPI/BL/BusinessPlanBL_exportDoc.cs
@@ -0,0 +1,5 @@
+﻿namespace KabadaAPI {
+  partial class BusinessPlanBL {
+    public string naceCode { get { return (o.Activity==null)?null:$"{o.Activity.Code} - {o.Activity.Title}"; }} 
+    }
+  }
