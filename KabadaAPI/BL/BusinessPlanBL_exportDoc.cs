3f62da92d355a6b7e9f10ce73ade8d4f630aba87
jst <Juris.Strods@sets.lv>
2021-12-02 14:28:50 +0200

    doc support

--- KabadaAPI/BL/BusinessPlanBL_exportDoc.cs
+++ KabadaAPI/BL/BusinessPlanBL_exportDoc.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using static KabadaAPI.Plan_AttributeRepository;
 
@@ -11,5 +12,32 @@ namespace KabadaAPI {
     public List<string> keyDist { get { return rvpL(PlanAttributeKind.keyDistributor); }}
     
     public List<string> keySupp { get { return rvpL(PlanAttributeKind.keySupplier); }}
+
+    protected List<string> _tValues(List<Guid> us){
+      var r=new List<string>();
+      if(us.Count>0){
+        var d=textSupport.get(us).ToDictionary(x=>x.Id);
+        r=us.Select(x=>d[x].Name).ToList(); 
+        }
+      return r;
+      }
+
+    protected List<string> _costs(List<CostBL> us){
+      var l= us.Select(x=>x.texterId).Distinct().ToList();
+      return _tValues(l);
+      }
+
+    public List<string> costFixed { get { return _costs(myFixedCost_s); }}
+
+    public List<string> costVariable { get { return _costs(myVariableCost_s); }}
+
+    public List<string> channels { get {
+      var w=gAv<ChannelElementBL>(PlanAttributeKind.channel).Select(x=>x.channel_type_id).Distinct().ToList();
+      return _tValues(w);
+      } }
+
+    //public List<string> ppp { get {
+    //  return null;
+    //  }}
     }
   }
