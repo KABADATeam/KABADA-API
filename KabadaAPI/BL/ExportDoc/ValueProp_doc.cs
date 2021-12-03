2d663f89e3f32e593ec89cec89f317ec0793fb4e
ennag <enna.golubova@sets.lv>
2021-12-03 14:13:42 +0200

    .

---
+++ KabadaAPI/BL/ExportDoc/ValueProp_doc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KabadaAPI
+{
+    public class ValueProp_doc
+    {
+        public string title;
+        public string prodType;
+        public string description;
+        public string priceLevel;
+        public string addIncomeSource; //comma separated string        
+        public List<string> productFeatures;
+        public string innovLevel; 
+        public string qualityLevel;
+        public string diffLevel;
+    }
+}
