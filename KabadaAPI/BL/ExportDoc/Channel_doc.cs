84ced3f78a64945146deb1dec2499b6c1afde739
ennag <enna.golubova@sets.lv>
2021-12-06 18:15:01 +0200

    Customer Relationship doc spec

---
+++ KabadaAPI/BL/ExportDoc/Channel_doc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KabadaAPI {
+  public class Channel_doc {
+        public string channelType;
+        public string distributionChannels; //comma separated list of names
+        public string products; //comma separated list of names
+    }
+
+}
---
+++ KabadaAPI/BL/ExportDoc/CustRel_doc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KabadaAPI {
+  public class CustRel_doc
+    {
+    public List<CustRelElement_doc> getCust;    
+    public List<CustRelElement_doc> keepCust;
+    public List<CustRelElement_doc> convCust;
+    }
+    public class CustRelElement_doc
+    {
+        public string action;
+        public string channel; //comma separated list of names
+    }
+}
