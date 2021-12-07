802a899870b17f1ebc91eaf7ac03c28a0a1aac58
ennag <enna.golubova@sets.lv>
2021-12-08 01:37:01 +0200

    keyPartners doc spec

---
+++ KabadaAPI/BL/ExportDoc/KeyPartners_doc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KabadaAPI {
+  public class KeyPartners_doc {
+    public List<KeyPartnersElement_doc> distributors;
+    public List<KeyPartnersElement_doc> suppliers;
+    public List<KeyPartnersElement_doc> others;
+    }
+  public class KeyPartnersElement_doc  {
+        public string partnerType;
+        public string company; 
+        public string priority; 
+        public string web; 
+        public string comment; 
+    }
+  
+}
