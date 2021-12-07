da262ff92bf5f3450a7ae95d4b360956b8ac3092
ennag <enna.golubova@sets.lv>
2021-12-07 15:32:08 +0200

    .

---
+++ KabadaAPI/BL/ExportDoc/KeyRes_doc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KabadaAPI {
+  public class keyRes_doc {       
+        public string category;
+        public string name; 
+        public string ownership; 
+        public string frequecy; 
+
+    }
+
+}
