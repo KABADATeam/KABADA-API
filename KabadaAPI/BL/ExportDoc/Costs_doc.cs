9b2f2c144357b0b5e9e7ae81f9c4cac180b37324
ennag <enna.golubova@sets.lv>
2021-12-08 08:14:52 +0200

    Costs doc spec Swot doc spec

---
+++ KabadaAPI/BL/ExportDoc/Costs_doc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KabadaAPI {
+  public class Costs_doc {
+        public List<CostElement_doc> fixedCosts;
+        public List<CostElement_doc> variableCosts;
+    }
+    public class CostElement_doc
+    {
+        public string category;
+        public string subCategory;
+        public string name;
+        public string desc;
+    }
+
+}
