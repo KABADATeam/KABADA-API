1c756351ef06fa5be532d3369988ea02ef85d36f
jst <Juris.Strods@sets.lv>
2021-12-10 11:39:19 +0200

    piu security increased (goes only from the same machine)

--- KabadaAPI/BL/Piu_shared.cs
+++ KabadaAPI/BL/Piu_shared.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 namespace KabadaAPI {
   partial class Piu {
-    private const string ActualKey="piu pieejas pārbaudīte";
+    private const string ActualKeyConst="piu pieejas pārbaudīte";
+
+    private static string ActualKey { get {
+      var server = Dns.GetHostName();
+      var heserver = Dns.GetHostEntry(server);
+      var al=heserver.AddressList.Select(x=>x.ToString()).OrderBy(x=>x).ToList();
+      var aa=string.Join(",", al);
+
+      return server+ActualKeyConst+aa;
+      }}
 
     public static string Parameter(string[] args){
       return ActualKey+args[0];
