f0ce9d21ff1cd82cc7d6ef889f300f4707ffb759
ennag <enna.golubova@sets.lv>
2021-12-14 17:00:25 +0200

    Revenue Streams Export PAge added Customer Segment Export page updated Industry Data PAge prepared

---
+++ KabadaAPI/Kabada/ExportPlan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kabada {
+  public class ExportPlan
+    {
+        public Guid id { get; set; }
+        public List<string> images { get; set; }
+       
+    }
+}
