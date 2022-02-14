c96919d8c9dc8792866afb4c4f9e269adb47e151
ennag <enna.golubova@sets.lv>
2022-02-14 14:32:28 +0200

    AI unload data (part1)

--- KabadaAPI/Kabada/BPunloaded.cs
+++ KabadaAPI/Kabada/BPunloaded.cs
@@ -1,4 +1,11 @@
-﻿namespace Kabada {
+﻿using KabadaAPI;
+using System;
+
+namespace Kabada {
   public partial class BPunloaded {
+    public Guid nace;
+    public Guid language;
+    public Guid country;
+    public CustomerSegmentAI custSegs;
     }
   }
---
+++ KabadaAPI/Kabada/CustomerSegmentAI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KabadaAPI {
+  public class CustomerSegmentAI {
+    public List<ConsumerSegmentAI> consumer;    
+    public List<BusinessSegmentAI> business;
+    public List<PublicSegmentAI> publicNgo; 
+    }
+  public class ConsumerSegmentAI {
+        public Guid segment_id;
+        public List<Guid> age; 
+        public List<Guid> gender; 
+        public List<Guid> education; 
+        public List<Guid> income; 
+        public List<Guid> geographic_location; 
+    }
+  public class BusinessSegmentAI {
+        public Guid segment_id;
+        public List<Guid> business_type; 
+        public List<Guid> company_size;    
+        public List<Guid> geographic_location; 
+    }
+    public class PublicSegmentAI
+    {
+        public Guid segment_id;
+        public List<Guid> business_type;
+    }
+}
