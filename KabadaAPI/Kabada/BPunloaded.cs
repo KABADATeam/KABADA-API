2e915fdad41c095097b34d7f14777256236b8d89
ennag <enna.golubova@sets.lv>
2022-02-14 16:30:13 +0200

    AI

--- KabadaAPI/Kabada/BPunloaded.cs
+++ KabadaAPI/Kabada/BPunloaded.cs
@@ -1,11 +1,23 @@
 ﻿using KabadaAPI;
 using System;
+using System.Collections.Generic;
 
 namespace Kabada {
   public partial class BPunloaded {
+    public Guid businessPlan_id;
     public Guid nace;
     public Guid language;
     public Guid country;
-    public CustomerSegmentAI custSegs;
+    public CustomerSegmentAI custSegs
+        {
+            get
+            {
+                var r = new CustomerSegmentAI();
+                //r.consumer = new List<ConsumerSegmentAI>() { new ConsumerSegmentAI() {} };
+                //r.business = new List<BusinessSegmentAI>() { new BusinessSegmentAI() {} };
+                //r.publicNgo = new List<PublicSegmentAI>() { new PublicSegmentAI() { } };
+                return r;
+            }
+        }
     }
   }
