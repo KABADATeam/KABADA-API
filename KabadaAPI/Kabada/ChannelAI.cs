d90fdd4f9b3dd402e614bb4c8fa3e5868a613b35
ennag <enna.golubova@sets.lv>
2022-02-25 10:45:52 +0200

    missed fields added

--- KabadaAPI/Kabada/ChannelAI.cs
+++ KabadaAPI/Kabada/ChannelAI.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 namespace Kabada {
   public class ChannelAI {       
         public Guid channelType;
+        public Guid? channelSubtype;
+        public Guid? subtypeType;
+        public Guid? locationType;
         public List<Guid> distributionChannels; 
         public List<Guid> products; //value proposition
     }
