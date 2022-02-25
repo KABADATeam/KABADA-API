969ea786d4fe67a5f675dcc7fa3e76800d258a7a
ennag <enna.golubova@sets.lv>
2022-02-25 11:07:32 +0200

    string ->bool

--- KabadaAPI/Kabada/KeyPartnerAI.cs
+++ KabadaAPI/Kabada/KeyPartnerAI.cs
@@ -10,7 +10,7 @@ namespace Kabada {
   public class KeyPartnersElementAI  {
         public Guid partnerType;
         public string company; 
-        public string priority; 
+        public bool priority; //yes,no
         public string web; 
         public string comment; 
     }
